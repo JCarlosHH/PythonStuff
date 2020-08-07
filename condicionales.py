0afb0e186017383dafb8c25a2fe8ba49f410cee6
Carlos Hernandez <carloshhyh@gmail.com>
2020-08-06 23:01:14 -0500

    Basic CRUD System v1

---
+++ condicionales.py
@@ -0,0 +1,7 @@
+edad=int(input("Escribe tu edad: "))
+if edad > 17 and edad <= 100:
+	print("Eres mayor de edad")
+elif edad > 100:
+	print("Probablemente ya estás muerto :v")
+elif edad < 17:
+	print("No eres mayor de edad")
