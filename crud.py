0a9d6e90ae9e4981138d034fd81f48166d888c19
Carlos Hernandez <carloshhyh@gmail.com>
2020-08-10 22:50:36 -0500

    Basic CRUD System with CSV File

--- crud.py
+++ crud.py
@@ -1,20 +1,27 @@
 import sys
+import csv
+import os
 
 
-clients = [
-	{
-		'name':'Bev',
-		'company': 'Google',
-		'email': 'bev@google.com',
-		'position': 'Software Engineer'
-	},
-	{
-		'name':'Bill',
-		'company': 'Google',
-		'email': 'bill@google.com',
-		'position': 'Data Engineer'
-	}
-]
+CLIENT_TABLE = '.clients.csv'
+CLIENT_SCHEMA = ['name','company','email','position']
+clients = []
+
+
+def _initialize_clients_from_storage():
+	with open(CLIENT_TABLE, mode='r') as f:
+		reader = csv.DictReader(f, fieldnames=CLIENT_SCHEMA)
+		for row in reader:
+			clients.append(row)
+
+
+def _save_clients_to_storage():
+	tmp_table_name = '{}.tmp'.format(CLIENT_TABLE)
+	with open(tmp_table_name,mode='w') as f:
+		writer = csv.DictWriter(f, fieldnames=CLIENT_SCHEMA)
+		writer.writerows(clients)
+	os.remove(CLIENT_TABLE)
+	os.rename(tmp_table_name, CLIENT_TABLE)
 
 
 def create_client(client):
@@ -78,6 +85,7 @@ def _print_welcome():
 	print('[U]pdate client')
 	print('[D]elete client')
 	print('[S]earch client')
+	print('[L]ist clients')
 	print('*'*50)
 	
 
@@ -101,6 +109,8 @@ def _get_cliet_name():
 
 
 if __name__ == '__main__':
+	_initialize_clients_from_storage()
+
 	_print_welcome()
 
 	command = input()
@@ -116,15 +126,13 @@ if __name__ == '__main__':
 		create_client(client)
 		list_clients()
 	elif command == 'D':
-		list_clients()
 		client_name = _get_client_field('name')
 		_get_client_delete(client_name)
+	elif command == 'L':
 		list_clients()
 	elif command == 'U':
-		list_clients()
 		updated_client_name = _get_cliet_name()
 		update_client(updated_client_name)
-		list_clients()
 	elif command == 'S':
 		client_name = _get_cliet_name()
 		found = search_client(client_name)
@@ -135,4 +143,5 @@ if __name__ == '__main__':
 	else:
 		print('Invalid command')
 
+	_save_clients_to_storage()
 
